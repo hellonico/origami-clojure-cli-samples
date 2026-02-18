bb94159c5a1663c593f0a6d20438d0fb26adf917
Nicolas Modrzyk <hellonico@gmail.com>
2026-02-18 12:42:30 +0100

    Fix preview: use grep -vF for (require filtering

--- run_example.sh
+++ run_example.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
     --prompt="Pick a Clojure Example: " \
     --height=80% \
     --border \
-    --preview "awk '/^\)/{found=1; next} found{print}' {} | grep -v '^\(require'")
+    --preview "awk '/^\)/{found=1; next} found{print}' {} | grep -vF '(require'")
   [ -z "$SCRIPT" ] && echo "No example selected." && exit 0
 else
   SCRIPT=$1
