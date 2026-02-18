4960142cdf651789d8cbe22ae18ca54c16f6f582
Nicolas Modrzyk <hellonico@gmail.com>
2026-02-18 14:28:48 +0100

    Add cam_queen.clj (interactive 4-pose booth) + update cam_snap.clj (-n multiple snaps)

---
+++ cam_queen.clj
@@ -0,0 +1,73 @@
+#!/bin/sh
+#_(
+
+DEPS='
+{:mvn/repos
+ {"vendredi" {:url "https://repository.hellonico.info/repository/hellonico/"}}
+ :deps
+ {origami/origami {:mvn/version "4.13.0-2-SNAPSHOT"}
+  org.clojure/clojure {:mvn/version "1.11.3"}
+  origami/filters {:mvn/version "1.48"}}}'
+
+exec clj -Sdeps "$DEPS" -M "$0" "$@"
+
+)
+(require '[opencv4.core :refer :all]
+         '[clojure.java.shell :refer [sh]])
+(import '[org.opencv.videoio VideoCapture])
+(import '[org.opencv.highgui HighGui])
+
+(def cap (VideoCapture. 0))
+
+(if (.isOpened cap)
+  (let [snaps (atom [])
+        start-time (System/currentTimeMillis)
+        last-snap (atom start-time)
+        frame (new-mat)]
+    
+    (println "Starting Queen Cam! Get ready for 4 poses!")
+    (println "Taking a picture every 2 seconds...")
+    
+    ;; Initial Loop
+    (loop []
+      (if (and (< (count @snaps) 4) (.read cap frame))
+        (do
+          (let [now (System/currentTimeMillis)
+                diff (- now @last-snap)]
+            
+            (if (> diff 2000)
+              (let [fname (str "queen_snap_" (count @snaps) ".png")]
+                (imwrite frame fname)
+                (swap! snaps conj fname)
+                (reset! last-snap now)
+                (println "📸 Snap!" (count @snaps) "-> " fname)
+                ;; Flash effect
+                (let [white (new-mat)] 
+                   (bitwise-not frame white) 
+                   (HighGui/imshow "Queen Cam" white)
+                   (HighGui/waitKey 100)))
+              (HighGui/imshow "Queen Cam" frame)))
+          
+          (if (not= 27 (HighGui/waitKey 30))
+            (recur)
+            (println "Aborted by user.")))
+        (println "Finished capturing.")))
+    
+    (.release cap)
+    (HighGui/destroyAllWindows)
+    
+    (if (= 4 (count @snaps))
+      (do
+        (println "Processing 👑 Queen Mosaic...")
+        (let [args (conj @snaps "queen_output.jpg")
+              _    (println "Running: ./cv_queen.clj" args) 
+              result (apply sh "./cv_queen.clj" args)]
+           (println (:out result))
+           (println (:err result))
+           (if (zero? (:exit result))
+             (do 
+               (println "Done! Saved to queen_output.jpg")
+               (try (sh "open" "queen_output.jpg") (catch Exception _)))
+             (println "Error in processing."))))
+      (println "Not enough pictures taken.")))
+  (println "Could not open camera."))
--- cam_snap.clj
+++ cam_snap.clj
@@ -1,33 +1,34 @@
 #!/bin/sh
 #_(
 
-   #_DEPS is same format as deps.edn. Multiline is okay.
-   DEPS='
-   {:mvn/repos
-   {"vendredi" {:url "https://repository.hellonico.info/repository/hellonico/"}}
-  :deps 
-   { origami/origami {:mvn/version "4.13.0-2-SNAPSHOT"}
-     org.clojure/clojure {:mvn/version "1.11.3"}
-     origami/filters {:mvn/version "1.48"}
-     clojure.java-time/clojure.java-time {:mvn/version "1.4.2"}
-     org.clojure/tools.cli {:mvn/version "1.1.230"}
-   }}
-   '
+DEPS='
+{:mvn/repos
+ {"vendredi" {:url "https://repository.hellonico.info/repository/hellonico/"}}
+ :deps
+ {origami/origami {:mvn/version "4.13.0-2-SNAPSHOT"}
+  org.clojure/clojure {:mvn/version "1.11.3"}
+  origami/filters {:mvn/version "1.48"}
+  clojure.java-time/clojure.java-time {:mvn/version "1.4.2"}
+  org.clojure/tools.cli {:mvn/version "1.1.230"}}}'
 
 exec clj -Sdeps "$DEPS" -M "$0" "$@"
 
 )
-
-(require 
- '[java-time.api :as time] 
+(require
+ '[java-time.api :as time]
+ '[clojure.string :as str]
  '[opencv4.core :refer [new-mat imwrite]]
- '[opencv4.utils :as u] 
+ '[opencv4.utils :as u]
  '[clojure.tools.cli :refer [parse-opts]])
 
 (def cli-options
   [["-c" "--cam VIDEO" "A descriptor for the cam" :default 0]
    ["-d" "--dir FOLDER" "A folder where to save the output" :default "out"]
-   ["-o" "--output FILE" "Output file" :default (str (time/format (time/formatter  "yyyy-MM-dd-HH:mm:ss") (time/local-date-time)) ".png")]
+   ["-o" "--output FILE" "Output file"
+    :default (str (time/format (time/formatter "yyyy-MM-dd-HH-mm-ss") (time/local-date-time)) ".png")]
+   ["-n" "--number N" "Number of pictures to take" 
+    :default 1 
+    :parse-fn #(Integer/parseInt %)]
    ["-h" "--help"]])
 
 (let [{:keys [options _ errors summary]} (parse-opts *command-line-args* cli-options)]
@@ -35,6 +36,18 @@ exec clj -Sdeps "$DEPS" -M "$0" "$@"
     (do (println "Usage: ./cam_snap.clj") (println summary))
     (if (not (nil? errors))
       (println errors)
-      (let [{:keys [cam output dir]} options]
+      (let [{:keys [cam output dir number]} options
+            _ (.mkdirs (clojure.java.io/as-file dir))]
         (println "Snap with options:" options)
-        (u/take-one cam (str dir "/" output))))))
+        (if (= 1 number)
+           (u/take-one cam (str dir "/" output))
+           (dotimes [i number]
+             (let [parts (str/split output #"\.")
+                   base  (str/join "." (butlast parts))
+                   ext   (last parts)
+                   fname (str base "_" i "." ext)
+                   path  (str dir "/" fname)]
+               (println "Taking snap" (inc i) "/" number " -> " path)
+               (u/take-one cam path)
+               ;; u/take-one takes some time to init cam, so delay is implicit
+               )))))))
